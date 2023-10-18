6ce8bd59ce9c1816f06304e2dd1b2c24b72fd252
NilayDas <mailnilaydas@gmail.com>
2023-10-19 02:22:08 +0530

    2nd commit

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,2 +1,4 @@
-#!/bin/bash
-java -jar -Dspring.profiles.active=dev /home/ec2-user/justiceleague-tracker-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
+#!/usr/bin/env bash
+cd /home/ec2-user/server
+sudo java -jar -Dserver.port=80 \
+    *.jar > /dev/null 2> /dev/null < /dev/null &
