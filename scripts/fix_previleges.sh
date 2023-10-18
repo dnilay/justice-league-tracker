689c943ec9ec7fc1b8e8d298d83df26feb63ea73
NilayDas <mailnilaydas@gmail.com>
2023-10-19 02:30:22 +0530

    2nd commit

--- scripts/fix_previleges.sh
+++ scripts/fix_previleges.sh
@@ -1,5 +1,5 @@
-#!/usr/bin/env bash
-chmod +x /home/ec2-user/server/* jar
-chmod +x /home/ec2-user/server/start_server.sh
-chmod +x /home/ec2-user/server/stop_server.sh
+#!/bin/bash
+chmod +x /home/ec2-user/justiceleague-tracker-0.0.1-SNAPSHOT.jar
+chmod +x /home/ec2-user/start_server.sh
+chmod +x /home/ec2-user/stop_server.sh
 
